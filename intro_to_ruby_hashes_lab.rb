1576ec11058a979bb7b957ee4223f00204f37455
Joseph Parker <jparker010190@gmail.com>
2018-04-07 20:29:14 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,6 +17,13 @@ def monopoly
 	      :two_pieces_owned : 50,
 	      :three_pieces_owned : 100,
 	      :four_pieces_owned : 200
+	    },
+	    :names :
+	    {
+	      :reading_railroad : {"mortgage_value" => "$100"},
+	      :pennsylvania_railroad : {"mortgage_value" => "$200"},
+	      :b_and_o_railroads : {"mortgage_value" => "$400"},
+	      :shortline : {"mortgage_value"
 	    }
 	  }
 	}
